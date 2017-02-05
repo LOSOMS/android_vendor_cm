e81d279243b275a44b1d257a4be16ac4e6e282b4
Joey Rizzoli <joey@lineageos.it>
2017-02-05 17:23:23 +0530

    cm: add LineageOS recorder
    * remove aosp soundrecorder and cm screencast
    * add only on full builds
    
    Change-Id: I68f7c62d2f78ce3d34e3ecaa554544d11fd7cc92
    Signed-off-by: Joey Rizzoli <joey@lineageos.it>

--- config/common.mk
+++ config/common.mk
@@ -134,8 +134,6 @@ PRODUCT_PACKAGES += \
     ExactCalculator \
     LiveLockScreenService \
     LockClock \
-    Screencast \
-    SoundRecorder \
     Trebuchet \
     WallpaperPicker \
     WeatherProvider
